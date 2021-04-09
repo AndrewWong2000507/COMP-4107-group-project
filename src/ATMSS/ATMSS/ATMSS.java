09f1e7dff8f38c3f17fb41837566b2a50816cc02
AndrewWong2000507 <wongandrew20000@gmail.com>
2021-04-10 01:45:26 +0800

    Amended ProcessKeyPress and added mode

--- src/ATMSS/ATMSS/ATMSS.java
+++ src/ATMSS/ATMSS/ATMSS.java
@@ -1,8 +1,12 @@
 package ATMSS.ATMSS;
 
+import ATMSS.BAMSHandler.BAMSHandler;
+import ATMSS.BAMSHandler.BAMSInvalidReplyException;
 import AppKickstarter.AppKickstarter;
 import AppKickstarter.misc.*;
 import AppKickstarter.timer.Timer;
+
+import java.io.IOException;
 
 
 //======================================================================
@@ -26,8 +30,16 @@ public class ATMSS extends AppThread {
     public String cardNo = "";
     public int pinCounter = 0;
     boolean outOfCash = false;
-    //Create BAMSHandler
+    private String userInput;
+    private String mode = "";
+    private String[] accNo = {"0", "1"}; //temp
 
+
+    //Create BAMSHandler
+    protected BAMSHandler bamsHandler;
+    private String urlPrefix = "http://cslinux0.comp.hkbu.edu.hk/comp4107_20-21_grp12/BAMS.php";
+
+    //Create ATM state
     ATMState atmState;
     boolean correctPinEntered = false;
 
@@ -46,15 +58,19 @@ public class ATMSS extends AppThread {
     } // ATMSS
 
     //Change ATM state
-    void setATMState(ATMState newATMState){
+    void setATMState(ATMState newATMState) {
         atmState = newATMState;
     }
 
-    public void resetCount(){
+    public void resetCount() {
         pinCounter = 0;
     }
-    public void resetPin(){ pin = "";}
-    public void resetAll(){
+
+    public void resetPin() {
+        pin = "";
+    }
+
+    public void resetAll() {
         resetCount();
         resetPin();
     }
@@ -67,14 +83,25 @@ public class ATMSS extends AppThread {
         atmState.ejectCard();
     }
 
-    public void insertPin(String CardNum, String Pin){
+    public void insertPin(String CardNum, String Pin) {
         atmState.insertPin(CardNum, Pin);
     }
 
-    public ATMState getYesCardState(){return hasCard;}
-    public ATMState getNoCardState(){return noCard;}
-    public ATMState getHasPin(){return hasCorrectPin;}
-    public ATMState getUnAvailable(){return unAvailable;}
+    public ATMState getYesCardState() {
+        return hasCard;
+    }
+
+    public ATMState getNoCardState() {
+        return noCard;
+    }
+
+    public ATMState getHasPin() {
+        return hasCorrectPin;
+    }
+
+    public ATMState getUnAvailable() {
+        return unAvailable;
+    }
 
     //end Change ATM state
 
@@ -91,10 +118,11 @@ public class ATMSS extends AppThread {
         cashDispenserMBox = appKickstarter.getThread("CashDispenserHandler").getMBox();
         buzzerMBox = appKickstarter.getThread("BuzzerHandler").getMBox();
         cashDepositCollectorMBox = appKickstarter.getThread("CashDepositCollectorHandler").getMBox();
-
+        bamsHandler = new BAMSHandler(urlPrefix);
+        bamsHandler.setLog(log);
 
         for (boolean quit = false; !quit; ) {
-            if(outOfCash){
+            if (outOfCash) {
                 atmState = unAvailable;
             }
             Msg msg = mbox.receive();
@@ -160,10 +188,20 @@ public class ATMSS extends AppThread {
     //------------------------------------------------------------
     // processKeyPressed
     private void processKeyPressed(Msg msg) {
+        //card inserted
         if (atmState == hasCard) {
             String key = msg.getDetails();
-            switch(key) {
-                case "0": case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9":
+            switch (key) {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
                     System.out.println("Key Pressed:" + key);
                     pin = pin + key;
                     System.out.println("Pin:" + pin);
@@ -174,14 +212,97 @@ public class ATMSS extends AppThread {
                     break;
                 case "Enter":
                     pinCounter++;
-                    if(pinCounter<=3){
+                    if (pinCounter <= 3) {
                         this.insertPin(cardNo, pin);
-                    }else{
+                    } else {
                         cardReaderMBox.send(new Msg(id, mbox, Msg.Type.CR_CardRemoved, "Card Locked"));
                     }
                     break;
                 case "Erase":
-                    pin = pin.substring(0, pin.length()-1);
+                    if (pin.length() > 0) {
+                        pin = pin.substring(0, pin.length() - 1);
+                    } else {
+                        log.info("ATMSS: PIN has nothing for erase");
+                    }
+                    break;
+                default:
+                    break;
+            }
+            //after login
+        } else if (atmState == hasCorrectPin) {
+            String key = msg.getDetails();
+            switch (key) {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    userInput = userInput + key;
+                    log.info("User Input : " + userInput);
+                    break;
+                case "Cancel":
+                    userInput = "";
+                    log.info("Process canceled");
+                    mode = "";
+                    break;
+                case "Enter":
+                    switch (mode) {
+                        case "cash withdrawal":
+                            //just some template
+                            try {
+                                if (Double.parseDouble(userInput) % 100 == 0 && Double.parseDouble(userInput) > 0) {
+                                    //only can get $100,$500 and $1000
+                                    bamsHandler.withdraw(cardNo, accNo[0], "", userInput);
+                                    mode = "";
+                                }
+                            } catch (BAMSInvalidReplyException | IOException e) {
+                                log.info("ATMSS: Error");
+                                mode = "";
+                            }
+                            break;
+                        case "cash transaction":
+                            //just some template
+                            try {
+                                if (Double.parseDouble(userInput) > 0) {
+                                    bamsHandler.transfer(cardNo, "", accNo[0], accNo[1], userInput);
+                                }
+                                //send to td transaction success
+                                log.info("ATMSS: transfer finished");
+                                mode = "";
+                            } catch (BAMSInvalidReplyException | IOException e) {
+                                log.info("ATMSS: Error");
+                                mode = "";
+                            }
+                            break;
+                        case "cash deposit":
+                            try {
+                                if (Double.parseDouble(userInput) > 0) {
+                                    bamsHandler.deposit(cardNo, accNo[0], "", userInput);
+                                }
+                                //send to td transaction success
+                                log.info("ATMSS: deposit finished");
+                                mode = "";
+                            } catch (BAMSInvalidReplyException | IOException e) {
+                                log.info("ATMSS: Error");
+                                mode = "";
+                            }
+                            break;
+                        default:
+                            log.info("ATMSS: Please choose the service type");
+                            //send the msg to display
+                    }
+                    break;
+                case "Erase":
+                    if (userInput.length() > 0) {
+                        userInput = userInput.substring(0, userInput.length() - 1);
+                    } else {
+                        log.info("ATMSS: User Input has nothing for erase");
+                    }
                     break;
                 default:
                     break;
@@ -204,7 +325,7 @@ public class ATMSS extends AppThread {
 
     //------------------------------------------------------------
     // processMouseClicked
-    private void processMouseClicked (Msg msg){
+    private void processMouseClicked(Msg msg) {
         // *** process mouse click here!!! ***
 
         String[] pos = msg.getDetails().trim().split("\\s+");
@@ -213,9 +334,11 @@ public class ATMSS extends AppThread {
         if (posX <= 300 && posY >= 415) {
             //cash deposit
             log.info("pressed cash deposit");
+            mode = "cash deposit";
         } else if (posX <= 300 && posY >= 345) {
             //cash withdrawal
             log.info("pressed cash withdrawal");
+            mode = "cash withdrawal";
         } else if (posX <= 300 && posY >= 275) {
             //cet account
             log.info("pressed get account");
@@ -228,6 +351,7 @@ public class ATMSS extends AppThread {
         } else if (posX >= 340 && posX <= 640 && posY >= 275) {
             //cash transaction
             log.info("pressed cash transaction");
+            mode = "cash transaction";
         }
 
     } // processMouseClicked
